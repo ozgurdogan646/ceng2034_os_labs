12ec3011a5f9c0e620aa5b35e9a49493710b1962
U170709026 <ozgurdogan646@gmail.com>
2019-03-03 02:42:01 +0300

    lab1 assigment

--- lab1/lab0.py
+++ lab1/lab0.py
@@ -1,14 +1,14 @@
 import os 
 import os.path , time
-os.chdir("/home")
+os.chdir("/home/ozgur")
 os.mkdir("os_lab_0")
-os.chdir("/home/os_lab_0")
+os.chdir("/home/ozgur/os_lab_0")
 open("first.txt","w")
 open("second.txt" , "w")
 open("third.py" , "w")
 print("Last modified of first.txt : %s" % time.ctime(os.path.getmtime("first.txt")))
 print("Last modified of second.txt : %s" % time.ctime(os.path.getmtime("second.txt")))
 print("Last modified of third.py : %s" % time.ctime(os.path.getmtime("third.py")))
-for file in os.listdir("/home/os_lab_0") :
+for file in os.listdir("/home/ozgur/os_lab_0") :
 	if file.endswith(".txt"):
-		print(os.path.join("/home" , file ))
+		print(os.path.join("/home/ozgur" , file ))
